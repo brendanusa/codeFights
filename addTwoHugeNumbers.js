0d0e0e5201c4b25697355545ebd64adbd5756e05
Brendan Bansavage <brendaniscool@gmail.com>
2018-01-03 20:32:28 -0500

    addTwoHugeNumbers almost complete

--- addTwoHugeNumbers.js
+++ addTwoHugeNumbers.js
@@ -1,45 +1,52 @@
 function addTwoHugeNumbers(a, b) {
 
-  var nodeToNumber = (node, string) => {
-    let stringPart = node.value.toString();
-    while (stringPart.length < 4) {
-      stringPart = '0' + stringPart;
-    }
-    string += stringPart;
-    if (node.next) {
-      return nodeToNumber(node.next, string);
-    } else {
-      console.log(parseInt(string));
-      return parseInt(string);
-    }
-  }
 
-  const sum = nodeToNumber(a, '') + nodeToNumber(b, '');
 
-  const removeLeadingZeroes = (string) => {
-    while (string[0] === '0' && string.length > 1) {
-      string[0] = '';
-    }
-  }
+  // var nodeToNumber = (node, string) => {
+  //   let stringPart = node.value.toString();
+  //   while (stringPart.length < 4) {
+  //     stringPart = '0' + stringPart;
+  //   }
+  //   string += stringPart;
+  //   if (node.next) {
+  //     return nodeToNumber(node.next, string);
+  //   } else {
+  //     console.log(parseInt(string));
+  //     return parseInt(string);
+  //   }
+  // }
 
-  const stringToNode = (num) => {
-    var string = num.toString();
-    // array?
-    var result = [];
-    let len = string.length;
-    while (string.length > 4) {
-      // string.slice??
-      let snippet = string[len - 4] + string[len - 3] + string[len - 2] + string[len - 1];
-      removeLeadingZeroes(snippet);
-      result.push(parseInt(snippet));
-    }
-    if (len) {
-      result.push(parseInt);
-    }
-    return result.reverse();
-  }
+  // const sum = nodeToNumber(a, '') + nodeToNumber(b, '');
 
-  return stringToNode(sum);
+  // const removeLeadingZeroes = (string) => {
+  //   while (string[0] === '0' && string.length > 1) {
+  //     string[0] = '';
+  //   }
+  // }
+
+  // const stringToNode = (num) => {
+  //   var string = num.toString();
+  //   // array?
+  //   var result = [];
+  //   let len = string.length;
+  //   while (string.length > 4) {
+  //     // string.slice??
+  //     let snippet = string[len - 4] + string[len - 3] + string[len - 2] + string[len - 1];
+  //     removeLeadingZeroes(snippet);
+  //     result.push(parseInt(snippet));
+  //   }
+  //   if (len) {
+  //     result.push(parseInt);
+  //   }
+  //   return result.reverse();
+  // }
+
+  // return stringToNode(sum);
 
 }
+
+var a = [9665]
+var b = [6933, 8443, 5132, 6809, 8519, 9096]
+
+console.log(addTwoHugeNumbers(a, b))
 
