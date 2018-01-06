99b886eea7fc3c9bdd49aff11c79316846859abc
Brendan Bansavage <brendaniscool@gmail.com>
2018-01-05 21:33:29 -0500

    complete groupingDishes

--- groupingDishes.js
+++ groupingDishes.js
@@ -1,18 +1,27 @@
 function groupingDishes(dishes) {
   var dishesByIngredient = {};
-  dishes.forEach(dish => {
-    dish.forEach((element, i) => {
-      if (i === 0) {
-      dishesByIngredient[dish] = [0];
-    }
-    if (dishesByIngredient[dish].length === 1 || element > dishesByIngredient[dish][dishesByIngredient[dish].length]) {
-      dishesByIngredient[dish].push(element);
-    } else {
-      let dishesIndexToCheck = dishesByIngredient[dish].length - 1;
-      while (element < dishesByIngredient[dish][dishesIndexToCheck]) {
-        dishesIndexToCheck--;
+  dishes.forEach(ingredientList => {
+    dish = ingredientList[0];
+    for (var i = 1; i < ingredientList.length; i++) {
+      if (!dishesByIngredient[ingredientList[i]]) {
+        dishesByIngredient[ingredientList[i]] = [ingredientList[i], dish];
+      } else {
+        dishesByIngredient[ingredientList[i]].push(dish);
       }
-      dishesByIngredient[dish][dishesIndexToCheck] = element;
     }
   })
+  return Object.values(dishesByIngredient).filter((ingredient) => {
+    return ingredient.length > 2;
+  }).sort();
 }
+
+dishes = [["Pasta","Tomato Sauce","Onions","Garlic"], 
+ ["Chicken Curry","Chicken","Curry Sauce"], 
+ ["Fried Rice","Rice","Onions","Nuts"], 
+ ["Salad","Spinach","Nuts"], 
+ ["Sandwich","Cheese","Bread"], 
+ ["Quesadilla","Chicken","Cheese"]]
+
+ // dishes = [["Salad","Tomato","Cucumber","Salad","Sauce"]]
+
+ console.log(groupingDishes(dishes))
