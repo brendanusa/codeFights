003d6a83e5b1a69d292b31e875345d24fcc08d8b
Brendan Bansavage <brendaniscool@gmail.com>
2018-01-11 12:01:11 -0800

    complete possibleSums, beautifully

--- possibleSums.js
+++ possibleSums.js
@@ -1,24 +1,23 @@
 function possibleSums(coins, quantity) {
   const sums = {};
 
-  for (let j = 0; j < coins.length; j++) {
-    let l = 1;
-    while (l <= quantity[j]) {
-      Object.keys(sums).forEach(sum => {
-        sums[parseInt(sum) + coins[j]] = null;
-      });
-      sums[coins[j]] = null;
-      l++;
+  for (let i = 0; i < coins.length; i++) {
+    // iteration of current coin
+    let j = 1;
+    // sums already populated from prev coins
+    let existingSums = Object.keys(sums).map(sum => parseInt(sum));
+    while (j <= quantity[i]) {
+      // add current coin value to existing sums
+      existingSums.forEach(sum => {
+        sums[coins[i] * j + sum] = null;
+      })
+      // add value to obj afterwards to avoid adding to itself
+      sums[coins[i] * j] = null;
+      j++;
     }
   }
-
-  // let str = '';
-  // Object.keys(sums).forEach(sum => {
-  //   str += sum + ', '
-  // })
-
-  // console.log(str)
-
+  
+  // number of unique sums
   return Object.keys(sums).length;
 }
 
@@ -28,8 +27,8 @@ quantity = [2, 2]
 // coins = [1, 2]
 // quantity = [50000, 2]
 
-coins = [1, 2, 3]
-quantity = [2, 3, 10000]
+// coins = [1, 2, 3]
+// quantity = [2, 3, 10000]
 
 // coins = [3, 1, 1]
 // quantity = [111, 84, 104]
