8ae410cf9e3505d0dfa6adfc9c031c1104c8fa23
Brendan Bansavage <brendaniscool@gmail.com>
2018-01-25 00:48:35 -0800

    complete isTreeSymmetric

--- isTreeSymmetric.js
+++ isTreeSymmetric.js
@@ -8,77 +8,162 @@
 
 function isTreeSymmetric(t) {
 
-    let result = true;
-
     if (!t) return true;
 
-    if (t.left && t.right && t.left.value !== t.right.value) {
-        console.log('16')
-        return result = false;
-    }
-    
-    const checkMiniTree = (node) => {
+    let result = true;
 
-        if (node.left && node.right) {
-            if (node.left.left && node.left.right && node.right.left && node.right.right) {
-                console.log('18')
-                if (node.left.left.value === node.right.right.value && node.left.right.value === node.right.left.value) {
-                    console.log('20')
-                    checkMiniTree(node.left);
-                    checkMiniTree(node.right);
-                } else {
-                    console.log('24')
-                    return result = false;
-                }
-            } else if (!node.left.left && !node.left.right && !node.right.left && !node.right.right) {
-                console.log('28')
-                return;
-            } else {
-                console.log('31')
-                return result = false;
-            }
-        } else if (!node.left && !node.right) {
-            console.log('35')
-            return;
+    const treeValues = [];
+
+    const checkLevel = (node, level) => {
+
+        treeValues[level] ? null : treeValues[level] = [];
+        
+        if (node.left) {
+            let leftLevel = level;
+            treeValues[leftLevel].push(node.left.value)
+            checkLevel(node.left, leftLevel + 1);
         } else {
-            console.log('38')
-            return result = false;
+            treeValues[level].push(null);
+        }
+
+        if (node.right) {
+            let rightLevel = level;
+            treeValues[rightLevel].push(node.right.value);
+            checkLevel(node.right, rightLevel + 1);
+        } else {
+            treeValues[level].push(null);
         }
 
     }
 
-    checkMiniTree(t);
+    checkLevel(t, 0)
+
+    let j = 0;
+
+    for (let i = 0; i < treeValues.length - 1; i++) {
+        j = 0;
+        while (j < treeValues[i].length / 2) {
+            // level[i] === level[level.length - 1 - i] ? null : result = false;
+            if (treeValues[i][j] !== treeValues[i][treeValues[i].length - 1 - j]) {
+                return result = false;
+            }
+            j++;
+        }
+
+    }
+
+    console.log(treeValues)
     
     return result;
     
 }
 
 t = {
-    "value": 1,
+    "value": -191,
     "left": {
-        "value": 2,
+        "value": 374,
         "left": {
-            "value": 3,
-            "left": null,
-            "right": null
+            "value": -361,
+            "left": {
+                "value": -771,
+                "left": null,
+                "right": {
+                    "value": -379,
+                    "left": {
+                        "value": -154,
+                        "left": null,
+                        "right": null
+                    },
+                    "right": {
+                        "value": -699,
+                        "left": null,
+                        "right": null
+                    }
+                }
+            },
+            "right": {
+                "value": 159,
+                "left": {
+                    "value": -900,
+                    "left": {
+                        "value": 305,
+                        "left": null,
+                        "right": null
+                    },
+                    "right": {
+                        "value": -486,
+                        "left": null,
+                        "right": null
+                    }
+                },
+                "right": {
+                    "value": 200,
+                    "left": {
+                        "value": -699,
+                        "left": null,
+                        "right": null
+                    },
+                    "right": {
+                        "value": 470,
+                        "left": null,
+                        "right": null
+                    }
+                }
+            }
         },
-        "right": {
-            "value": 4,
-            "left": null,
-            "right": null
-        }
+        "right": null
     },
     "right": {
-        "value": 2,
-        "left": {
-            "value": 4,
-            "left": null,
-            "right": null
-        },
+        "value": 374,
+        "left": null,
         "right": {
-            "value": 3,
-            "left": null,
-            "right": null
+            "value": -361,
+            "left": {
+                "value": 159,
+                "left": {
+                    "value": 200,
+                    "left": {
+                        "value": 470,
+                        "left": null,
+                        "right": null
+                    },
+                    "right": {
+                        "value": -699,
+                        "left": null,
+                        "right": null
+                    }
+                },
+                "right": {
+                    "value": -900,
+                    "left": {
+                        "value": -486,
+                        "left": null,
+                        "right": null
+                    },
+                    "right": {
+                        "value": 305,
+                        "left": null,
+                        "right": null
+                    }
+                }
+            },
+            "right": {
+                "value": -771,
+                "left": {
+                    "value": -379,
+                    "left": {
+                        "value": -699,
+                        "left": null,
+                        "right": null
+                    },
+                    "right": {
+                        "value": -154,
+                        "left": null,
+                        "right": null
+                    }
+                },
+                "right": null
+            }
         }
     }
 }
