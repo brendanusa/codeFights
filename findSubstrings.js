1f2ef7fc5abf7418ecfbe2789d1b79a6787bf4bc
Brendan Bansavage <brendaniscool@gmail.com>
2018-02-16 15:57:23 -0800

    still too slow

--- findSubstrings.js
+++ findSubstrings.js
@@ -7,6 +7,7 @@ function findSubstrings(words, parts) {
     let i = 0;
     let index;
     const matches = {};
+    let matchExists = false;
     while (i < parts.length) {
       index = word.indexOf(parts[i]);
       if (index !== -1) {
@@ -14,38 +15,67 @@ function findSubstrings(words, parts) {
           matches[parts[i].length] = {};
         }
         matches[parts[i].length][index] = word.replace(parts[i], `[${parts[i]}]`);
+        matchExists = true;
+      }
+      if (matchExists) {
+        if (i === parts.length - 1 || parts[i].length > parts[i + 1].length) {
+          console.log(parts[i], matches[parts[i].length])
+          return matches[parts[i].length][Object.keys(matches[parts[i].length]).sort((a, b) => a - b)[0]];
+        }
       }
       i++;
-    }
-    if (Object.keys(matches).length > 0) {
-      return matches[Object.keys(matches).sort((a, b) => b - a)[0]][Object.keys(matches[Object.keys(matches).sort((a, b) => b - a)[0]]).sort((a, b) => a - b)[0]]
     }
     return word;
   })
   return result;
+
+    // OLD RETURN
+    // if (Object.keys(matches).length > 0) {
+    //   return matches[Object.keys(matches).sort((a, b) => b - a)[0]][Object.keys(matches[Object.keys(matches).sort((a, b) => b - a)[0]]).sort((a, b) => a - b)[0]]
+    // }
+
+// // EVEN SLOWER!
+//   // sort parts by length
+//   partsObj = {};
+//   parts.forEach(part => {
+//     if (!partsObj[part.length]) {
+//       partsObj[part.length] = [];
+//     }
+//     partsObj[part.length].push(part);
+//   })
+
+//   const partLengths = Object.keys(partsObj).sort((a, b) => b - a);
+//   console.log(partsObj)
+
+//   words = words.map(word => {
+//     const matches = {};
+//     for (let i = 0; i < partLengths.length; i++) {
+//       console.log('word', word)
+//       console.log('partLength', partLengths[i])
+//       partsObj[partLengths[i]].forEach(part => {
+//         const matchIndex = word.indexOf(part);
+//         if (matchIndex !== -1) {
+//           matches[matchIndex] = word.replace(part, `[${part}]`);
+//         }
+//       })
+//       const matchIndexes = Object.keys(matches).sort((a, b) => a - b);
+//       if (matchIndexes.length > 0) {
+//         return matches[matchIndexes[0]];
+//       }
+//     }
+//     return word;
+//   })
+
+//   return words;
+
 }
 
-
-
-words = ["Apple", 
- "Melon", 
- "Orange", 
- "Watermelon"]
-parts = ["a", 
- "mel", 
- "lon", 
- "el", 
- "An"]
-
- output =
-["Apple", 
- "Me[lon]", 
- "Or[a]nge", 
- "Waterme[lon]"]
-expectedOutput =
-["Apple", 
- "Me[lon]", 
- "Or[a]nge", 
- "Water[mel]on"]
+words = ["Aaaaaaaaa", 
+ "bcdEFGh"]
+parts = ["aaaaa", 
+ "Aaaa", 
+ "E", 
+ "z", 
+ "Zzzzz"]
 
  console.log(findSubstrings(words, parts));
